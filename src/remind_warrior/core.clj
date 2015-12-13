184f6b91efb7d5600e4e632dbd167a5d522b393e
jeaye <contact@jeaye.com>
2015-12-13 18:02:26 +0800

    Show project

--- src/remind_warrior/core.clj
+++ src/remind_warrior/core.clj
@@ -14,13 +14,18 @@
                        (tf/formatters :basic-date-time-no-ms) time-str)
                      (t/default-time-zone)))))
 
+(defn make-description [task]
+  (str (when (:project task)
+         (str "(" (:project task) ") "))
+       (:description task)))
+
 (defn task->rem [task]
   (let [todo [(str "REM " (parse-time (:entry task))
-                   " *1 MSG " (:description task))]]
+                   " *1 MSG " (make-description task))]]
     (if-let [due (:due task)]
       (conj todo (str "REM " (parse-time due)
                       " AT " (parse-time due "HH:mm")
-                      " MSG Due: " (:description task)))
+                      " MSG Due: " (make-description task)))
       todo)))
 
 (defn -main
