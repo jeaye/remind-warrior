28ffa72bef6cc0bafd2187c89c8c45b82747b9fd
jeaye <contact@jeaye.com>
2015-12-13 16:41:12 +0800

    Add arity overload to parse-time

--- src/remind_warrior/core.clj
+++ src/remind_warrior/core.clj
@@ -2,19 +2,22 @@
   (:gen-class)
   (:require [clojure.java.shell :as shell]
             [clj-time.format :as tf]
+            [clojure.pprint :as pp]
             [clojure.data.json :as json]))
 
-(defn parse-time [time-str form] ; TODO: Overload on arity with default form
-  (tf/unparse
-    (tf/formatter form)
-    (tf/parse (tf/formatters :basic-date-time-no-ms) time-str)))
+(defn parse-time
+  ([time-str] (parse-time time-str "MMMM d yyyy"))
+  ([time-str form]
+   (tf/unparse
+     (tf/formatter form)
+     (tf/parse (tf/formatters :basic-date-time-no-ms) time-str))))
 
 (defn task->rem [task]
-  (pprint/pprint task)
-  (let [todo [(str "REM " (parse-time (:entry task) "MMMM d yyyy")
+  ;(pp/pprint task)
+  (let [todo [(str "REM " (parse-time (:entry task))
                    " *1 MSG " (:description task))]]
     (if-let [due (:due task)]
-      (conj todo (str "REM " (parse-time due "MMMM d yyyy")
+      (conj todo (str "REM " (parse-time due)
                       " AT " (parse-time due "HH:mm")
                       " MSG Due: " (:description task)))
       todo)))
@@ -23,7 +26,7 @@
   [& args]
   (let [tasks (->> (json/read-str (:out (shell/sh "task" "export"))
                                    :key-fn keyword)
-                    (filter #(not= (:status %) "deleted"))
+                    (filter #(= (:status %) "pending"))
                     (sort-by :urgency)
                     reverse)
         rems (mapcat task->rem tasks)]
