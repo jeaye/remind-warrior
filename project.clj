b56414bab49787c6da26bcd842dea625d1404c3a
jeaye <contact@jeaye.com>
2015-12-13 19:04:34 +0800

    Update the project info

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject remind-warrior "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject remind-warrior "0.1.0"
+  :description "TaskWarrior -> Remind converter"
+  :url "https://github.com/jeaye/remind-warrior"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/data.json "0.2.6"]
                  [clj-time "0.11.0"]]
