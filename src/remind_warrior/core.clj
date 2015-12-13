a910c5dd2dc088f6d7b76fbae46f958a90e0b6c2
jeaye <contact@jeaye.com>
2015-12-13 14:03:27 +0800

    Begin mapping tasks into rem

--- src/remind_warrior/core.clj
+++ src/remind_warrior/core.clj
@@ -4,11 +4,15 @@
             [clojure.pprint :as pprint]
             [clojure.data.json :as json]))
 
+(defn task->rem [task]
+  [(str "REM " (:entry task) " MSG " (:description task))])
+
 (defn -main
   [& args]
-  (let [parsed (->> (json/read-str (:out (shell/sh "task" "export"))
+  (let [tasks (->> (json/read-str (:out (shell/sh "task" "export"))
                                    :key-fn keyword)
                     (filter #(not= (:status %) "deleted"))
                     (sort-by :urgency)
-                    reverse)]
-    (pprint/pprint (apply list parsed))))
+                    reverse)
+        rems (mapcat task->rem tasks)]
+    (pprint/pprint (apply list rems))))
