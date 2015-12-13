87c73154006de9fe07de2faed60e040432c7184e
jeaye <contact@jeaye.com>
2015-12-13 14:24:11 +0800

    Add proper time parsing

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [org.clojure/data.json "0.2.6"]
+                 [clj-time "0.11.0"]]
   :main ^:skip-aot remind-warrior.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/remind_warrior/core.clj
+++ src/remind_warrior/core.clj
@@ -2,10 +2,16 @@
   (:gen-class)
   (:require [clojure.java.shell :as shell]
             [clojure.pprint :as pprint]
+            [clj-time.format :as tf]
             [clojure.data.json :as json]))
 
+(defn parse-time [time-str]
+  (tf/unparse
+    (tf/formatter "MMMM d yyyy")
+    (tf/parse (tf/formatters :basic-date-time-no-ms) time-str)))
+
 (defn task->rem [task]
-  [(str "REM " (:entry task) " MSG " (:description task))])
+  [(str "REM " (parse-time (:entry task)) " MSG " (:description task))])
 
 (defn -main
   [& args]
